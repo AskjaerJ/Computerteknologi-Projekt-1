7cafcfb152742f91f1645f4c8aea62094681b78d
jacobrask15 <49784405+jacobrask15@users.noreply.github.com>
2023-02-01 19:34:27 +0100

    2 + test done

--- array_queue/array_int_queue.cpp
+++ array_queue/array_int_queue.cpp
@@ -11,30 +11,32 @@ Array_int_queue::Array_int_queue(int size)
 void Array_int_queue::enqueue(int x)
 {
     assert(front < max);
-    for (int i = front-1; i >= 0; i--) {
-        queue[i+1] = queue[i];
+    for (int i = front - 1; i >= 0; i--)
+    {
+        queue[i + 1] = queue[i];
     }
     queue[0] = x;
     front++;
 }
 
-int Array_int_queue::dequeue() 
+int Array_int_queue::dequeue()
 {
     assert(front > 0);
     front--;
     return queue[front];
 }
 
-bool Array_int_queue::empty() 
+bool Array_int_queue::empty()
 {
-    return front > 0;
+    return front == 0;
 }
 
-bool Array_int_queue::full() 
+bool Array_int_queue::full()
 {
     return front == max;
 }
 
-Array_int_queue::~Array_int_queue() {
+Array_int_queue::~Array_int_queue()
+{
     delete queue;
 }
