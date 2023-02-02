004f3e080f46140ac028d82d7870cef46057a550
AskjaerJ <askjaer36@gmail.com>
2023-02-02 09:27:42 +0100

    Tests setup

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,12 +3,40 @@
 #include <assert.h>
 #include "array_int_queue.h"
 #include "int_queue.h"
+#include "list_int_queue.h"
 
 // See Catch2's documentation: https://github.com/catchorg/Catch2/blob/devel/docs/tutorial.md#scaling-u
 
 TEST_CASE("array_queue")
 {
     Array_int_queue test(10);
+    REQUIRE(test.empty() == true);
+    test.enqueue(99);
+    test.enqueue(4);
+    test.enqueue(-5);
+    REQUIRE(test.dequeue() == 99);
+    REQUIRE(test.dequeue() == 4);
+    REQUIRE(test.dequeue() == -5);
+    REQUIRE(test.empty() == true);
+
+    /*Fills up queue*/
+    test.enqueue(99);
+    test.enqueue(4);
+    test.enqueue(-5);
+    test.enqueue(99);
+    test.enqueue(4);
+    test.enqueue(-5);
+    test.enqueue(99);
+    test.enqueue(4);
+    test.enqueue(-5);
+    REQUIRE(test.full() == false);
+    test.enqueue(99);
+    REQUIRE(test.full() == true);
+}
+
+TEST_CASE("list_queue")
+{
+    List_int_queue test(10);
     REQUIRE(test.empty() == true);
     test.enqueue(99);
     test.enqueue(4);
