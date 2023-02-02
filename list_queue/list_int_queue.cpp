f49fbeeef74862ef0ccae86e5e31773afed93318
AskjaerJ <askjaer36@gmail.com>
2023-02-02 15:06:33 +0100

    hahah

--- list_queue/list_int_queue.cpp
+++ list_queue/list_int_queue.cpp
@@ -14,18 +14,18 @@ void List_int_queue::enqueue(int x)
     qnode *new_qnode; /**/
     new_qnode->data = x;
 
-    if (q->size == 0)
+    if (size == 0)
     { /*both front and rear must be new node*/
-        q->front = new_qnode;
-        q->rear = new_qnode;
+        front = new_qnode;
+        rear = new_qnode;
     }
     else
     {
-        q->rear->next = new_qnode; /*includes new node in queue*/
-        q->rear = new_qnode;       /*updates rear to be newly inserted node*/
+        rear->next = new_qnode; /*includes new node in queue*/
+        rear = new_qnode;       /*updates rear to be newly inserted node*/
     }
 
-    q->size++;
+    size++;
 }
 
 int List_int_queue::dequeue()
@@ -34,18 +34,18 @@ int List_int_queue::dequeue()
     int x = q->front->data; /*saves data to be returned*/
     qnode *temp = q->front; /*temporary qnode pointer to hold node to be freed*/
 
-    q->front = q->front->next; /*sets new front*/
+    front = q->front->next; /*sets new front*/
 
     free(temp); /*to avoid memory leak*/
 
-    q->size--;
+    size--;
 
     return x;
 }
 
 bool List_int_queue::empty()
 {
-    return q->size == 0; /*queue is empty when size is 0*/
+    return size == 0; /*queue is empty when size is 0*/
 }
 
 bool List_int_queue::full()
